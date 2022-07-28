1f5b94b058222246b02c969a8c49e083f9edae80
DESKTOP-V0H394L\User <e107ti@etec.sp;gov;br>
2022-07-28 14:53:12 -0300

    Starting the app and doing the front

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <Text style={styles.Tittle}>Previsão do tempo</Text>
+      </View>
+      <View>
+        <Text style={styles.Label}>Cidade</Text>
+      </View>
+      <View>
+      <TextInput style={styles.Input}
+        placeholder='Insira aqui sua cidade'
+        />
+      </View>
+      <View style={styles.bloco}>
+        <TouchableOpacity style={styles.botao}>
+          <Text style={styles.textoBotao}>Buscar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>  
   );
 }
 
@@ -17,4 +31,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  Input: {
+    border: "#000",
+  },
+  botao: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "#6800C9",
+    width: 80,
+  },
+  textoBotao: {
+    color: "#fff",
+  }
 });
